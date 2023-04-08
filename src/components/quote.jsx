80944e8b69d03c747ebf0628710070320c94ca52
Josh Fernando <112506062+JoshFernando@users.noreply.github.com>
2023-04-07 23:23:28 -0400

    newquote

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -11,8 +11,8 @@ const Quote = () => {
       variants={fadeIn("right", "spring",0, 10)}
       className="flex-[0.75] bg-black-100 p-8 rounded-2xl justify-center content-center"
     >
-      <h1 className={`${styles.heroSubText} text-center`}>"The mind that opens to a new idea never returns to its original size."</h1>
-      <h2 className="font-bold text-[#ff8000] text-center mt-3">- Albert Einstein</h2>
+<h1 className={`${styles.heroSubText} text-center`}>Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.</h1>
+      <h2 className="font-bold text-green-500 text-center mt-3">- Albert Einstein</h2>
     </motion.div>
   );
 };
